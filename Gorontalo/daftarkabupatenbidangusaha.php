7b44659bc6e426c994f18004b21a60c0f93ba960
agungdwiasmoro <dwiagung591@gmail.com>
2021-07-13 11:16:14 +0700

    penambahan provinsi

---
+++ Gorontalo/daftarkabupatenbidangusaha.php
@@ -0,0 +1,15 @@
+			<td>
+              <div class="btn-group">
+                <button type="button" class="btn btn-default btn-xs btn-block dropdown-toggle" data-toggle="dropdown">Pilih Kabupaten/Kota
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a href="../../Kabupaten Boalemo/index.php"> Kab. Boalemo</a></li>
+                  <li><a href="../../Kabupaten Bone Bolango/index.php"> Kab. Bone Bolango</a></li>
+                  <li><a href="../../Kabupaten Gorontalo/index.php"> Kab. Gorontalo</a></li>
+                  <li><a href="../../Kabupaten Gorontalo Utara/index.php"> Kab. Gorontalo Utara</a></li>
+                  <li><a href="../../Kabupaten Pohuwato/index.php"> Kab. Pohuwato</a></li>
+                  <li><a href="../../Kota Gorontalo/index.php"> Kota Gorontalo</a></li>
+                </ul>
+              </div>
+              </td>
